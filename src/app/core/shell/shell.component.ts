c1963bec6a002c090009ed1f0f6d63a984dc1426
Walecki Maciej <maciej.walecki@agroapp.com.pl>
2023-09-03 01:16:38 +0200

    feat: added footer

--- src/app/core/shell/shell.component.ts
+++ src/app/core/shell/shell.component.ts
@@ -5,12 +5,13 @@ import { HeroComponent } from '../hero/hero.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { GlobalLoaderService } from '../global-loader/global-loader.service';
 import { AsyncPipe, NgIf } from '@angular/common';
+import { FooterComponent } from '@app/core/footer/footer.component';
 
 @Component({
   selector: 'c-shell',
   standalone: true,
   template: `
-    <div>
+    <div class="shell">
       <mat-progress-bar
         *ngIf="isLoading$ | async"
         mode="indeterminate"
@@ -19,8 +20,10 @@ import { AsyncPipe, NgIf } from '@angular/common';
       <main>
         <router-outlet></router-outlet>
       </main>
+      <c-footer />
     </div>
   `,
+  styles: ['.shell {min-height: 100vh; position: relative}'],
   imports: [
     RouterOutlet,
     NavbarComponent,
@@ -28,6 +31,7 @@ import { AsyncPipe, NgIf } from '@angular/common';
     MatProgressBarModule,
     AsyncPipe,
     NgIf,
+    FooterComponent,
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
