aa3fa0db27574fa130b3700ea08bfeefcabcf4b7
Maciej Walecki <maciejwalecki99@gmail.com>
2023-10-08 16:56:07 +0200

    build: added production environment config

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 import { Environment } from '@src/environments/environment.model';
 export const environment: Environment = {
-  API_URL: 'http://localhost:3000/api/v1',
+  API_URL: 'https://cocktails-backend-sj6m.onrender.com/api/v1',
 }
