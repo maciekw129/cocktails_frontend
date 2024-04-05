d07b2d7a495324679b960eb6dc89dcea2fccf169
Maciej Walecki <maciejwalecki99@gmail.com>
2024-04-05 20:29:03 +0200

    feat: added cocktail fallback image

--- apps/cocktails/src/app/modules/user-profile/components/my-cocktails/my-cocktails.component.ts
+++ apps/cocktails/src/app/modules/user-profile/components/my-cocktails/my-cocktails.component.ts
@@ -6,13 +6,12 @@ import { Cocktail } from '@app/modules/cocktails/cocktails.model';
 @Component({
   selector: 'c-my-cocktails',
   standalone: true,
-  imports: [],
   templateUrl: './my-cocktails.component.html',
   styleUrls: ['./my-cocktails.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MyCocktailsComponent {
   cocktails$: Observable<Cocktail[]> = inject(ActivatedRoute).data.pipe(
-    map(({ cocktails }: { cocktails: Cocktail[] }) => cocktails)
+    map(({ cocktails }) => cocktails)
   );
 }
